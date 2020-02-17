56e3d463935273af1a497a77c31cecd710aa74f9
Alexander <avmolinapro@gmail.com>
2020-02-17 17:19:49 +0100

    Se genero un nuevo componente nombre:persona

---
+++ src/app/personas.model.ts
@@ -0,0 +1,11 @@
+export class Persona{
+    // nombre: string;
+    // apellidos:string;
+
+    // constructor(nombre:string, apellidos:string){
+    //     this.nombre = nombre;
+    //     this.apellidos = apellidos;
+    // }
+    constructor(public nombre:string,public apellidos:string){}
+    
+}
